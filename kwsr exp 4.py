21760ae12aaf4f1c9a87207209696f56895ca589
kawsarshk <kawsarshk9907@gmail.com>
2026-03-06 23:01:02 +0530

    Add files via upload

---
+++ kwsr exp 4.py
@@ -0,0 +1,7 @@
+
+# A program to calculate simple interest
+Principal=float(input("Enter the principal amount:"))
+Rate=float(input("Enter the amount of rate:"))
+Time=float(input("Enter the time period:"))
+Simple_Interest=(Principal*Rate*Time)/100
+print("The Simple Interest is:",Simple_Interest)
